cb549909539ceae888762b516dc648563a45b227
Michael Wiktorek <wiktorekmichael@gmail.com>
2018-01-27 16:59:13 -0800

    removed immutable tick code from game

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -14,23 +14,6 @@ export interface DrawDatum {
 export class Game {
     constructor(private rules: RuleSet) {}
 
-    public tick = (state: GameState) => {
-        const { grid, bug } = state;
-        const color = getSquare(grid, bug.position);
-        const rule = this.rules[color];
-        const newDirection = rotate(bug.direction, rule.rotation);
-        const newColor = rule.color;
-        const newPosition = this.updateBug(
-            { direction: newDirection, position: bug.position },
-            grid
-        );
-        const newState = {
-            bug: { direction: newDirection, position: newPosition },
-            grid: this.immutableUpdateGrid(grid, bug.position, newColor)
-        };
-        return newState;
-    };
-
     public mutableTick = (state: GameState) => {
         const { grid, bug } = state;
         const color = getSquare(grid, bug.position);
@@ -50,29 +33,4 @@ export class Game {
     private updateBug = (bug: Bug, grid: Grid) => {
         return translate(bug.position, bug.direction);
     };
-
-    private immutableUpdateGrid(
-        grid: Grid,
-        bugPosition: Coordinate,
-        color: GridSquareColor
-    ): Grid {
-        const size = grid.data.length;
-        const newGrid: GridSquareColor[][] = [];
-        for (let i = 0; i < size; i++) {
-            if (bugPosition.x === i) {
-                const newRow: GridSquareColor[] = [];
-                for (let j = 0; j < size; j++) {
-                    if (j === bugPosition.y) {
-                        newRow.push(color);
-                    } else {
-                        newRow.push(grid.data[i][j]);
-                    }
-                }
-                newGrid[i] = newRow;
-            } else {
-                newGrid[i] = grid.data[i];
-            }
-        }
-        return { data: newGrid };
-    }
 }
